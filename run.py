597890cecec5222421abe81c8263241976f15973
Caleb Liu <lakelxx@hotmail.com>
2018-05-03 19:38:49 -0700

    Database done

--- run.py
+++ run.py
@@ -9,7 +9,8 @@
     :license:
 """
 
-from FlaskApp import app
+from FlaskApp import app, db
+from FlaskApp.models import User, Post
 
 """
 How to start the flask app
@@ -20,6 +21,10 @@ set FLASK_APP = run.py
 BASH:
 export FLASK_APP = run.py
 """
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
 
 if __name__ == '__main__':
     app.run(debug=True)
